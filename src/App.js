1af6aadbec766b4cb6394117f321148baab991a7
Shubham <shubhamadke@gmail.com>
2022-05-11 19:01:32 +0530

    Added Style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="hero">
+        <div className="content">
+          <h1>
+            <span className="firstLetter">S</span>hree
+            <span className="firstLetter"> S</span>ai
+            <span className="firstLetter"> E</span>lectronics
+          </h1>
+          <h3>
+            <i>Everything that is broken can be fixed!</i>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
