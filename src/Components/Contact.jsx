db2ec7a158acf47dd97de1ddac1f98c3de0a7595
Shubham <shubhamadke@gmail.com>
2022-05-17 23:03:59 +0530

    All done except responsiveness

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FormBox from "./FormBox";
 import Map from "./Map";
+import Phone from "./Phone";
 
 function Contact() {
   return (
     <div id="contact" className="container">
       <FormBox />
+      <Phone />
       <Map />
     </div>
   );
---
+++ src/Components/Phone.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Phone() {
+  return (
+    <div className="phone">
+      <h2 className="text-center bg-dark">CONTACT</h2>
+      <div className="d-flex justify-content-center">
+        <div className="row">
+          <div className="col-sm-6">
+            <h3>+91 9423268948</h3>
+          </div>
+          <div className="col-sm-6">
+            <h3>+91 9405266316</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Phone;
