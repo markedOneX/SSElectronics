10ed988b9370e7a240e69619188d9a98d8a4c69f
Shubham <shubhamadke@gmail.com>
2022-05-20 21:51:48 +0530

    Final Commit 1.1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import portrait from "../Design/portrait2.jpg";
+import portrait from "../Design/Portrait.jpg";
 
 function About() {
   return (
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Card from "./Card";
+import DataRec from "../Design/DataRec.png";
+import Legacy from "../Design/Legacy.png";
+import Others from "../Design/Others.png";
+import PhotoCam from "../Design/PhotoCam.png";
+import TV from "../Design/TV.png";
+import VideoCam from "../Design/VideoCam.png";
 
 function Services() {
   return (
@@ -9,27 +15,21 @@ function Services() {
           <Card
             title={"LED/LCD"}
             desc={"Services for LED/LCD TVs, Monitors, Smart TVs"}
-            src={
-              "https://cdn.pixabay.com/photo/2013/07/12/14/49/flatscreen-148843_960_720.png"
-            }
+            src={TV}
           />
         </div>
         <div className="col-sm-4">
           <Card
             title={"Video Camera"}
             desc={"Services for Camcorders, Professional Video Cameras"}
-            src={
-              "https://cdn.pixabay.com/photo/2016/04/01/12/20/camera-1300668_960_720.png"
-            }
+            src={VideoCam}
           />
         </div>
         <div className="col-sm-4">
           <Card
             title={"Photo Camera"}
             desc={"Services for Digital Cameras, Compact Cameras, DSLR"}
-            src={
-              "https://cdn.pixabay.com/photo/2017/02/01/10/47/avatars-2029585_960_720.png"
-            }
+            src={PhotoCam}
           />
         </div>
       </div>
@@ -38,9 +38,7 @@ function Services() {
           <Card
             title={"Legacy Hardware"}
             desc={"We also repair old legacy hardware such as VCR, VHS, Radios"}
-            src={
-              "https://cdn.pixabay.com/photo/2019/05/07/16/55/stereo-system-4186417_960_720.png"
-            }
+            src={Legacy}
           />
         </div>
         <div className="col-sm-4">
@@ -49,9 +47,7 @@ function Services() {
             desc={
               "Memory Cards, Harddisks, SSDs, Pen Drives. We'll recover your lost data for you."
             }
-            src={
-              "https://cdn.pixabay.com/photo/2012/04/12/20/42/hard-30576_960_720.png"
-            }
+            src={DataRec}
           />
         </div>
         <div className="col-sm-4">
@@ -60,9 +56,7 @@ function Services() {
             desc={
               "Did we miss something? Let us know your requirement and we'll figure it out."
             }
-            src={
-              "https://cdn.pixabay.com/photo/2013/07/13/12/42/stereo-system-160146_960_720.png"
-            }
+            src={Others}
           />
         </div>
       </div>
